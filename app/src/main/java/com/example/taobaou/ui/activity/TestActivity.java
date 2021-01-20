9e9ca3774cefeed34bc7d72673b2143d1ee5b49f
jaepeng <pzy163163163@163.com>
2021-01-20 08:56:21 +0800

    可以进入到RegisterAndRecognizeActivity,但是没有激活引擎过

--- app/src/main/java/com/example/taobaou/ui/activity/TestActivity.java
+++ app/src/main/java/com/example/taobaou/ui/activity/TestActivity.java
@@ -1,9 +1,11 @@
 package com.example.taobaou.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.facelibs.activity.RegisterAndRecognizeActivity;
 import com.example.taobaou.R;
 import com.example.taobaou.base.BaseActivity;
 import com.example.taobaou.model.domain.UserInfo;
@@ -35,9 +37,11 @@ public class TestActivity extends BaseActivity {
 
     @OnClick(R.id.btn_test_sp)
     public void setData(){
-        UserInfo userInfo=new UserInfo("1234");
-        SharedPreferenceManager.getInstance().setLastUser(userInfo);
-        boolean firstLogin = SharedPreferenceManager.getInstance().isFirstLogin();
+        Intent intent=new Intent(TestActivity.this, RegisterAndRecognizeActivity.class);
+        startActivity(intent);
+//        UserInfo userInfo=new UserInfo("1234");
+//        SharedPreferenceManager.getInstance().setLastUser(userInfo);
+//        boolean firstLogin = SharedPreferenceManager.getInstance().isFirstLogin();
     }
     @OnClick(R.id.btn_test_sp_get)
     public void getData(){
---
+++ facelibs/src/main/java/com/example/facelibs/util/face/RequestLivenessStatus.java
@@ -0,0 +1,5 @@
+package com.example.facelibs.util.face;
+
+public class RequestLivenessStatus {
+    public static final int ANALYZING = 10;
+}
